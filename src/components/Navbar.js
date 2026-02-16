f818b1fa04109752641a22ac904baca38437de70
shivamshete1036 <159644256+shivamshete1036@users.noreply.github.com>
2026-02-16 23:08:15 +0530

    Navbar section created

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css'; // We will create this next
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h2>MyPortfolio</h2>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
